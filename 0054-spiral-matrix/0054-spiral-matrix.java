adcee424c6e5620e951576decb5869492c917d9b
HarshSingh2727 <iamharshsingh1@gmail.com>
2026-08-23 05:50:50 -0700

    Time: 0 ms (100%), Space: 43 MB (44.1%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,46 +1,41 @@
-import java.util.*;
-
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
+         List<Integer> ans = new ArrayList<>();
 
-        List<Integer> ans = new ArrayList<>();
+         int n = matrix.length ;
+         int m = matrix[0].length ;
 
-        int top = 0;
-        int bottom = matrix.length - 1;
-        int left = 0;
-        int right = matrix[0].length - 1;
+         int top = 0 ; 
+         int bottom = n-1 ;
+         int left = 0 ;
+         int right = m-1 ;
 
-        while (top <= bottom && left <= right) {
+         while(top<= bottom && left <= right){
 
-            // Left -> Right
-            for (int j = left; j <= right; j++) {
-                ans.add(matrix[top][j]);
+         for(int i = left ; i <= right ; i++){
+            ans.add(matrix[top][i]) ;
+         }
+         top ++ ;
+
+          for(int i = top ; i <= bottom  ; i++){
+            ans.add(matrix[i][right]) ;
+         }
+         right-- ;
+
+        if(top <= bottom){
+            for(int i = right ; i >= left ; i--){
+                ans.add(matrix[bottom][i]);
             }
-            top++;
-
-            // Top -> Bottom
-            for (int i = top; i <= bottom; i++) {
-                ans.add(matrix[i][right]);
-            }
-            right--;
-
-            // Right -> Left
-            if (top <= bottom) {
-                for (int j = right; j >= left; j--) {
-                    ans.add(matrix[bottom][j]);
-                }
-                bottom--;
-            }
-
-            // Bottom -> Top
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
-                    ans.add(matrix[i][left]);
-                }
-                left++;
-            }
+            bottom -- ;
         }
 
-        return ans;
+        if(left<= right){
+            for(int i = bottom ; i >= top ; i--){
+                ans.add(matrix[i][left]) ;
+            }
+            left++ ;
+        }
+        }
+    return ans ;   
     }
 }
